a1e4e36ab769abb22dc01ff1939be3865e338952
git-dmitriy <shalberkind@mail.ru>
2021-07-27 23:55:34 +0300

    Create form for a new product

---
+++ src/Products/ProductEdit.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { css } from '@emotion/css';
+
+const ProductEditStyles = css`
+  color: #fff;
+  background-color: #2a2c37;
+  border-radius: 6px;
+  padding: 15px;
+`;
+
+const ProductEdit = () => {
+  return <div>I&apos;m product edit component</div>;
+};
+
+export default ProductEdit;
