19a2ba453491fc664074e50eaa929e8dc5a147c4
git-dmitriy <shalberkind@mail.ru>
2021-07-27 23:46:39 +0300

    Stylize the admin page

--- src/Admin/Admin.js
+++ src/Admin/Admin.js
@@ -1,4 +1,42 @@
 import React from 'react';
-const Admin = () => <div>Admin</div>;
+import { Link, Route, Routes } from 'react-router-dom';
+import ProductsIndex from '../Products/ProductsIndex';
+import ProductEdit from '../Products/ProductEdit';
+import { css } from '@emotion/css';
+
+const AdminStyles = css`
+  .Admin {
+    &-Header {
+      display: flex;
+      align-items: center;
+    }
+
+    &-New {
+      text-decoration: none;
+      border: 2px solid #fff;
+      color: #fff;
+      padding: 4px 10px;
+      border-radius: 6px;
+      font-weight: 600;
+      text-transform: uppercase;
+      margin-left: auto;
+    }
+  }
+`;
+
+const Admin = () => (
+  <div className={AdminStyles}>
+    <div className='Admin-Header'>
+      <h1>Admin</h1>
+      <Link to='new' className='Admin-New'>
+        Create new
+      </Link>
+    </div>
+    <Routes>
+      <Route path='/' element={<ProductsIndex />} />
+      <Route path='/new' element={<ProductEdit />} />
+    </Routes>
+  </div>
+);
 
 export default Admin;
