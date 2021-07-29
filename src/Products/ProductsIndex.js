98df001eb25a607ba9490c46231447c6d89c3df2
git-dmitriy <shalberkind@mail.ru>
2021-07-29 23:30:46 +0300

    Syncing Search Param State with UI State

--- src/Products/ProductsIndex.js
+++ src/Products/ProductsIndex.js
@@ -1,11 +1,42 @@
 import React, { useState, useEffect } from 'react';
 import { listProducts } from './ProductService';
 import ProductCard from './ProductCard';
-import { useLocation } from 'react-router';
+import { useLocation, useSearchParams } from 'react-router-dom';
+import { css } from '@emotion/css';
+
+const ProductsIndexStyles = css`
+  .ProductsIndex {
+    &-Radios {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      span {
+        width: 35px;
+        color: #fff;
+        font-size: 0.8rem;
+        margin-right: 10px;
+      }
+      label {
+        display: flex;
+        flex-direction: row-reverse;
+        cursor: pointer;
+      }
+      input {
+        cursor: pointer;
+      }
+    }
+    &-List {
+      padding: 0;
+      list-style: none;
+      margin-top: 10px;
+    }
+  }
+`;
 
 const ProductsIndex = () => {
   const { state } = useLocation();
   const [products, setProducts] = useState(null);
+  const [searchParams, setSearchParams] = useSearchParams();
   let controller = new AbortController();
 
   useEffect(() => {
@@ -23,15 +54,71 @@ const ProductsIndex = () => {
     return () => controller?.abort();
   }, []);
 
+  const updateParams = (e) => {
+    const { name, value } = e.target;
+    const currentParams = Object.fromEntries([...searchParams]);
+    const newParams = { ...currentParams, [name]: value };
+    setSearchParams(newParams);
+  };
+
   if (products === null) {
     return <div>Loading...</div>;
   }
   return (
-    <ul style={{ padding: '0', listStyle: 'none' }}>
-      {products.map((item) => (
-        <ProductCard product={item} key={item.id} />
-      ))}
-    </ul>
+    <div className={ProductsIndexStyles}>
+      <div className='ProductsIndex-Radios'>
+        <span>Sort:</span>
+        <label>
+          Name
+          <input
+            type='radio'
+            name='sort'
+            value='name'
+            onChange={updateParams}
+            defaultChecked={searchParams.get('sort') === 'name'}
+          />
+        </label>
+        <label>
+          Price
+          <input
+            type='radio'
+            name='sort'
+            value='price'
+            onChange={updateParams}
+            defaultChecked={searchParams.get('sort') === 'price'}
+          />
+        </label>
+      </div>
+      <div className='ProductsIndex-Radios'>
+        <span>Order:</span>
+        <label>
+          Ascending
+          <input
+            type='radio'
+            name='order'
+            value='ascending'
+            onChange={updateParams}
+            defaultChecked={searchParams.get('order') === 'ascending'}
+          />
+        </label>
+        <label>
+          Descending
+          <input
+            type='radio'
+            name='order'
+            value='descending'
+            onChange={updateParams}
+            defaultChecked={searchParams.get('order') === 'descending'}
+          />
+        </label>
+      </div>
+
+      <ul className='ProductsIndex-List'>
+        {products.map((item) => (
+          <ProductCard product={item} key={item.id} />
+        ))}
+      </ul>
+    </div>
   );
 };
 
